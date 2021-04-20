c6ed90a0836c56c492fc898613c6ffaababa82bc
rahul mutyalkar <rahulmutyalkar@gmail.com>
2021-04-20 16:41:59 +0530

    search thing is done

--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -1,35 +1,42 @@
-
-import { Component, OnInit } from '@angular/core';
-import { GiphyService } from '../services/giphy.service';
+import { Component, OnInit } from "@angular/core";
+import { GiphyService } from "../services/giphy.service";
 
 @Component({
-  selector: 'app-search-page',
-  templateUrl: './search-page.component.html',
-  styleUrls: ['./search-page.component.scss']
+  selector: "app-search-page",
+  templateUrl: "./search-page.component.html",
+  styleUrls: ["./search-page.component.scss"],
 })
 export class SearchPageComponent implements OnInit {
-
-  imageList=[];
-  constructor(private giphyService:GiphyService) { }
+  imageList = [];
+  categoryList = [
+    "Graphics",
+    "Addons",
+    "Buttons",
+    "Infographics",
+    "PSDs",
+    "Fonts",
+    "Forms",
+    "Graphs",
+  ];
+  constructor(private giphyService: GiphyService) {}
 
   ngOnInit() {
-    this.searchApi()
+    this.searchApi();
   }
 
-searchApi()
-{
-  this.giphyService.searchGif('sports').subscribe((response:any)=>{
-    console.log('response : ',response.data);
-    this.imageList = response.data;
-  },(error)=>{
-    // console.log('error : ',error);
-    this.imageList = [];
-  })
-}
+  searchApi() {
+    this.giphyService.searchGif("sports").subscribe(
+      (response: any) => {
+        console.log("response : ", response.data);
+        this.imageList = response.data;
+      },
+      (error) => {
+        this.imageList = [];
+      }
+    );
+  }
 
-onLoad(event,imageObj,index) {
-  // console.log('onLoad : ',index);
-  imageObj.load=true;
-}
-
+  onLoad(event, imageObj, index) {
+    imageObj.load = true;
+  }
 }
